7e26783feeb4caa09785d004bb394ab72053532d
jmiego95 <johnmiego@gmail.com>
2022-06-17 23:41:40 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+Code = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+user_input = input("What would you like to translate to morse code: ")
+
+
+def string_to_morse(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += Code[letter] + ' '
+        else:
+            cipher += ' '
+    return cipher
+
+
+def main():
+    message = user_input
+    result = string_to_morse(message.upper())
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
